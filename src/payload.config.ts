eec5f38eba0a9c65e5b52b44b70701c47c7f4555
VEDA95 <snetterfield1@gmail.com>
2026-02-06 01:47:19 -0500

    Included base settings for live preview

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -36,6 +36,29 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
+  livePreview: {
+    collections: ['pages'],
+    breakpoints: [
+      {
+        label: 'Mobile',
+        name: 'mobile',
+        width: 375,
+        height: 667,
+      },
+      {
+        label: 'Tablet',
+        name: 'tablet',
+        width: 768,
+        height: 1024,
+      },
+      {
+        label: 'Desktop',
+        name: 'desktop',
+        width: 1440,
+        height: 900,
+      },
+    ],
+  },
   collections: [Pages, Posts, Media, Categories, Users],
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
