ae2c6d19596417d60f05c25eba100cdd9423c86d
VEDA95 <snetterfield1@gmail.com>
2026-02-06 23:03:05 -0500

    WIP troubleshooting 500 server errors

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,4 +1,12 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from '@opennextjs/cloudflare/config'
 
+if (typeof process !== 'undefined' && !process.report?.getReport) {
+  // @ts-ignore
+  process.report = {
+    ...process.report,
+    getReport: () => ({}),
+  }
+}
+
 export default defineCloudflareConfig({})
